66f91e273d56342708d8b69ad85b3c77c79878c4
ayamohammed112 <ayamohammedabbas112@gmail.com>
2026-06-25 18:23:30 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -97,7 +97,7 @@ export default function Home() {
           style={{
             position: 'absolute',
             top: 0,
-            left: 0,
+            left: 155,
             width: '100%',
             height: '100%',
             objectFit: 'cover',
